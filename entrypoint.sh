9cd7bbda3fa4530f5c31f646410b917ef04f3867
Tedd Ho-Jeong An <tedd.an@intel.com>
2022-06-30 23:25:28 -0700

    Add workspace folder to git safe directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,13 @@ if [ -z "$SECRET_TOKEN" ]; then
     exit 1
 fi
 
+echo "Add the workspace dir to GIT safe directory"
+git config --global --add safe.directory $GITHUB_WORKSPACE
+
+echo "Add user name and email to global config"
+git config --global user.name "$GITHUB_ACTOR"
+git config --global user.email "$GITHUB_ACTOR@users.noreply.github.com"
+
 if [ "$FOR_UPSTREAM_BRANCH" = 'none' ]; then
     /sync_repo.sh "$@"
 else
